092f4402eb29ea4cddb4d4c013a34d6ac4a2cc17
Eugene Birondo <jbirondo@gmail.com>
2020-10-12 19:35:47 -0700

    Picker api

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ import flask
 import flask_cors
 from flask import request, jsonify, json
 from flask_cors import CORS
+import unicodedata
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = False
@@ -19,43 +20,54 @@ def get_main_data():
     c = r.content
     soup = BeautifulSoup(c, "html.parser")
     all = soup.find_all("table", {"class": "frodds-data-tbl"})
-    weatherr = requests.get("https://rotogrinders.com/weather/nfl", headers={
+    weatherr = requests.get("https://www.vegasinsider.com/nfl/weather/", headers={
                             'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
+
+
     weatherc = weatherr.content
     weathersoup = BeautifulSoup(weatherc, "html.parser")
-    weatherall = weathersoup.find_all("div", {"class": "crd"})
+
+    first_table = weathersoup.find("tbody")
+    games_table = first_table.find("tbody")
+    games = games_table.find_all("tr")
+    games = games[1:-1]
+
     weatherd = {}
-    for tab in weatherall:
-        teams = [x.text.replace(" ", "_")
-                 for x in tab.find_all("span", {"class": "lng"})]
+    for game in games:
+        teams = ["_".join(x.text.strip().split()[:-1]) if x.text.strip()
+                != "Washington Football Team" else "Washington" for x in game.find_all("b")]
         for i in range(0, 2):
-            if teams[i] == "New_York":
-                teams[i] = "N.Y._" + \
-                    [x.text for x in tab.find_all("span", {"class": "mascot"})][i]
             if teams[i] == "Los_Angeles":
-                teams[i] = "L.A._" + \
-                    [x.text for x in tab.find_all("span", {"class": "mascot"})][i]
-        weathertable = [x.text for x in tab.find_all("span", {"class": "display"})]
-        weathertable = weathertable[1:3] + weathertable[4:]
-        if len(weathertable) > 1:
-            weatherd[teams[1].replace(" ", "_")] = {
-                "Temperature": weathertable[0],
-                "Precipitation": weathertable[1],
-                "Wind Direction": weathertable[2],
-                "Wind Speed": weathertable[3]
-            }
-        else:
-            weatherd[teams[1].replace(" ", "_")] = {
-                "Temperature": "Dome",
-                "Precipitation": "Dome",
-                "Wind Direction": "Dome",
-                "Wind Speed": "Dome"
-            }
-    table = all[0].find_all("tr")
+                teams[i] = "L.A._" + game.find_all("b")[i].text.strip().split()[-1]
+            if teams[i] == "New_York":
+                teams[i] = "N.Y._" + game.find_all("b")[i].text.strip().split()[-1]
+        weathertable = [x.text.replace("\n", "").replace(
+            "\r", "").replace("\t", "") for x in game.find_all("td")]
+        heat_index = unicodedata.normalize("NFKC", weathertable[2:][-1])
+        humidity = unicodedata.normalize("NFKC", weathertable[2:][-2])
+        temperature = unicodedata.normalize(
+            "NFKC", weathertable[2:][-3]) if unicodedata.normalize("NFKC", weathertable[2:][-3]) != " " else "DOME"
+        wind = unicodedata.normalize("NFKC", weathertable[2:][-4]).split()
+        wind_direction = " ".join(wind[:-1]) if wind else "DOME"
+        wind_speed = "{} mph".format(wind[-1].replace(".", "")) if wind else "DOME"
+        summary = weathertable[2] if weathertable[2] != "\xa0" else "NO SUMMARY"
+        weatherd[teams[1]] = {
+            "Summary": summary,
+            "Wind Direction": wind_direction,
+            "Wind Speed": wind_speed,
+            "Temperature": temperature,
+            "Humidity": humidity,
+            "Heat Index": heat_index
+        }
+
+
+    table = all[0].find_all("tr")[:len(weatherd)]
     l = []
-    for a in table[:len(weatherd)]:
+    for a in table:
         d = {}
         teams = [x.text.replace(" ", "_") for x in a.find_all("b")]
+        if teams[1] not in weatherd.keys():
+            continue
         odds = a.find_all("td", {"class": "oddsCell"})
         o = odds[1].find_all("a")
         ol = []
@@ -78,9 +90,10 @@ def get_main_data():
         d["Home"] = teams[1]
         d["Date/Time"] = a.find("span", {"class": "cellTextHot"}).text
         d["Temperature"] = weatherd[teams[1]]["Temperature"]
-        d["Precipitation"] = weatherd[teams[1]]["Precipitation"]
+        # d["Precipitation"] = weatherd[teams[1]]["Precipitation"]
         d["Wind Direction"] = weatherd[teams[1]]["Wind Direction"]
         d["Wind Speed"] = weatherd[teams[1]]["Wind Speed"]
+        d["Summary"] = weatherd[teams[1]]["Summary"]
         l.append(d)
     sortedArray = sorted(
         l,
@@ -109,8 +122,8 @@ def get_injury_data():
                 "span", {"class": "CellPlayerName--long"}).text.replace("\n", "")
             p["Position"] = arr[0]
             p["Date of Injury"] = " ".join(arr[1:4])
-            p["Injury"] = arr[4]
-            p["Injury Summary"] = " ".join(arr[5:])
+            p["Injury"] = player.find("td", {"style": " width: 20%;"})
+            p["Injury Summary"] = player.find("td", {"style":" min-width: 200px; width: 40%;"})
             injd[teamname].append(p)
         injd[teamname] = sorted(injd[teamname], key=lambda x: datetime.strptime(
             x['Date of Injury'], '%a, %b %d'), reverse=True)
